b10ae4be0c494bad374342151606dac2c5ec84c3
BioHazard <hazardland@gmail.com>
2012-10-16 18:18:08 +0400

    lighting up

--- src/hazardland/notes/Notes.java
+++ src/hazardland/notes/Notes.java
@@ -7,7 +7,15 @@ import hazardland.notes.lib.Adapter;
 import hazardland.notes.lib.Note;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 
 public class Notes extends Activity
@@ -25,6 +33,7 @@ public class Notes extends Activity
 		notes = new ArrayList <Note> ();
 		notesAdapter = new Adapter (Notes.this, R.layout.item, notes);
 		notesList.setAdapter (notesAdapter);
+		registerForContextMenu(notesList);
 		
 		String[] files = Files.list ("/sdcard/biohazard/oneonote/");
 		
@@ -61,4 +70,54 @@ public class Notes extends Activity
 		notes.add (note);
 	}
 
+	@Override
+	public void onCreateContextMenu (ContextMenu menu, View view, ContextMenuInfo menuInfo) 
+	{
+	    super.onCreateContextMenu (menu, view, menuInfo);
+	    //menu.setHeaderTitle (getString(R.string.menu_context_title));
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.view, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected (MenuItem item) 
+	{
+	    AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	    switch (item.getItemId()) 
+	    {
+		    	case R.id.view_delete:
+					AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+					 
+					// set title
+					//alertDialogBuilder.setTitle("Your Title");
+		 
+					// set dialog message
+					alertDialogBuilder
+						.setMessage("Delete note "+notes.get((int) info.id).name+"?")
+						.setCancelable(false)
+						.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int id) {
+							}
+						  })
+						.setNegativeButton("No",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int id) {
+								dialog.cancel();
+							}
+						});
+		 
+						// create alert dialog
+						AlertDialog alertDialog = alertDialogBuilder.create();
+		 
+						// show it
+						alertDialog.show();
+					
+		
+		
+	    		return true;
+		    	default:
+		        return super.onContextItemSelected(item);
+	    }
+	}
+	
+	
 }
